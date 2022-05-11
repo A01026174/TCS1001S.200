48daa50526db693a2e779b8ceaa82b10a48ee146
Samantha Covarrubias <a01026174@tec.mx>
2022-05-11 12:17:40 -0500

    Add python file

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+"""
+Simple test
+Samantha Covarrubias
+"""
+def main():
+    print("Hii lovely human!!")
+    print("You are amazing!!")
+
+    for i in range(8):
+        print("Keep it up!! You've got this!!")
+
+main()
